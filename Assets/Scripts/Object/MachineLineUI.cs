e7a87c44d59619cc5ee88d0247b50a378c2ac120
chiffonpie <loadkyu@gmail.com>
2022-10-12 12:18:15 +0900

    스프라이트 교체형 버튼 처리 작업

--- Assets/Scripts/Object/MachineLineUI.cs
+++ Assets/Scripts/Object/MachineLineUI.cs
@@ -16,12 +16,15 @@ namespace ThousandLines
         private TextMeshProUGUI m_Price;
 
         [SerializeField]
-        private Button m_BuyButton;
+        private SpriteButton m_Settingbutton;
+
         [SerializeField]
-        private Button m_Settingbutton;
+        private SpriteButton m_BuyButton;
 
         public void Initialize(MachineLineData machineLineData)
         {
+            //모든 버튼의 초기화
+            this.SetButtonSprites(this.m_BuyButton, machineLineData);
             //구매 여부 체크
             if (machineLineData.Line_isGet == 0)
             {
@@ -37,19 +40,31 @@ namespace ThousandLines
             if (machineLineData.Line_isActive == 0)
             {
                 // 설치할 수 있는 버튼 이벤트 추가
-                this.m_BuyButton.OnClickAsObservable().Subscribe(_ =>
+                this.m_Settingbutton.OnClickAsObservable().Subscribe(_ =>
                 {
                     this.OnClickSettingButton();
                 });
             }
         }
 
+        private void SetButtonSprites(SpriteButton button, MachineLineData machineLineData)
+        {
+            //리소스 로드로 스프라이트 세팅을 함
+
+            button.NormalSprite = null;
+            button.spriteState = new SpriteState()
+            {
+                highlightedSprite = null,
+                pressedSprite     = null,
+                selectedSprite    = null,
+                disabledSprite    = null,
+            };
+        }
 
         private void OnClickBuyButton()
         {
             Debug.LogError(11);
         }
-
 
         private void OnClickSettingButton()
         {
