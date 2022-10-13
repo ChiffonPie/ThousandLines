bf4d3b18d802f064dcd28b65d2334a81624ea07c
chiffonpie <loadkyu@gmail.com>
2022-10-13 17:09:34 +0900

    데이터 수정 및 설계주석 추가

--- Assets/Scripts/Object/GoalMachine.cs
+++ Assets/Scripts/Object/GoalMachine.cs
@@ -38,8 +38,31 @@ namespace ThousandLines
         protected override void ReadySequence()
         {
             base.ReadySequence();
+
+            //리스트 체크하고 정리한다.
             //다음 머신이 존재하는지 여부에 따라서 먼저 재배치 한다.
 
+            //누가 추가되어 대기중인지 확인
+            if (ThousandLinesManager.Instance.m_InMachines.Count > this.SettingIndex)
+            {
+                // 널체크 부터
+
+                // 순서
+                // 1. 내 뒤에 Null 있는지 확인 후 압축
+                // 2. 널이 아닌경우 인데 추가된 사항이 있는경우
+                //    - 해당 항목을 In 으로 대체시키고 자신은 뒤로 빠진다.
+                // 할만한데?
+
+                if (true)
+                {
+
+                }
+            }
+
+            if (true)
+            {
+
+            }
             //머신 리스트를 재정리 한다.
 
             ThousandLinesManager.Instance.MachineSend(this);
